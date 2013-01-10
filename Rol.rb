6f6f1e893698b1d7f6442cc0f4be587755a120ec
Lucio Assis <luciolucio@gmail.com>
2013-01-10 01:19:02 -0200

    Adicionei texto de exemplo que vou usar pra desenvolver

--- Rol.rb
+++ Rol.rb
@@ -5,19 +5,26 @@ require 'mail'
 require_relative 'lib/mailer' 
 require_relative 'lib/processor' 
 
-config = YAML.load( File.new( 'config.y' ) )
+def get_text_for_messages
+	config = YAML.load( File.new( 'config.y' ) )
 
-Mail.defaults do
-	retriever_method :imap, {
-		:address    => 'imap.gmail.com',
-		:port       => 993,
-		:enable_ssl => true,
-		:user_name  => config[ :user ],
-		:password   => config[ :pass ],
-	}
+	Mail.defaults do
+		retriever_method :imap, {
+			:address    => 'imap.gmail.com',
+			:port       => 993,
+			:enable_ssl => true,
+			:user_name  => config[ :user ],
+			:password   => config[ :pass ],
+		}
+	end
+
+	emails = Rol::Mailer.get_all
+	emails.each do | email |
+		puts email[ :body ]
+	end
 end
 
-emails = Rol::Mailer.get_all
-emails.each do | email |
-	puts email[ :body ]
-end
+# get_text_for_messages
+
+text = File.new( "out.txt" ).read
+puts text
