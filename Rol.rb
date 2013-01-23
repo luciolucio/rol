89c4faff01291941db01a3de5bcd8eb472c95755
Lucio Assis <luciolucio@gmail.com>
2013-01-23 00:45:31 -0200

    Parser agora funciona com mais de um cartao

--- Rol.rb
+++ Rol.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/ruby
 
-require 'ruby-debug'
 require 'yaml'
 require 'bigdecimal'
 require 'mail'
@@ -8,7 +7,7 @@ require_relative 'lib/mailer'
 require_relative 'lib/processor' 
 
 def read_text_from_file
-	file = File.new( "out2.txt" )
+	file = File.new( "out3.txt" )
 	full_text = file.read
 
 	return [ full_text ]
@@ -39,26 +38,31 @@ def parse( messages )
 	results = []
 
 	messages.each do | full_text |
-		entradas = full_text.scan( /^(\d\d-\d\d-\d\d\d\d)\s+(.*?)\s+R\$\s([0-9,.]+).*$/ )
-		entradas.each do | entrada |
-			data, estabelecimento, valor = entrada
-			data = Date.parse( data )
-			valor = BigDecimal.new( valor.gsub( ",", "virgula" ).gsub( ".", "," ).gsub( "virgula", "." ) )
+		bandeira = full_text.scan( /CARTAO PERSONNALITE (.*) PLATINUM/ )[ 0 ]
+		lines = full_text.split( "\n" )
 
-			result = { "data" => data, "estab" => estabelecimento, "valor" => valor }
-			results.push result
+		cartao_final = 0
+
+		lines.each do | line |
+			numero = line.scan( /final ...(\d*)/ )[ 0 ]
+			if numero.nil? && cartao_final == 0
+				next
+			elsif !numero.nil?
+				cartao_final = numero
+			else
+				entrada = line.scan( /^(\d\d-\d\d-\d\d\d\d)\s+(.*?)\s+R\$\s([0-9,.]+).*$/ )[ 0 ]
+				if entrada.nil?
+					next
+				else
+					data, estabelecimento, valor = entrada
+					data = Date.parse( data )
+					valor = BigDecimal.new( valor.gsub( ",", "virgula" ).gsub( ".", "," ).gsub( "virgula", "." ) )
+					result = { "bandeira" => bandeira, "cartao" => cartao_final, "data" => data, "estab" => estabelecimento, "valor" => valor }
+
+					results.push result
+				end
+			end
 		end
-
-		#bandeira = full_text.scan( /CARTAO PERSONNALITE (.*) PLATINUM/ )[ 0 ]
-
-		#lines = full_text.split( "\n" )
-
-		#debugger
-		#current = 0
-		#lines.each do | line |
-			#numero = line.scan( /final ...(\d*)/ )[ 0 ]
-			#if !numero.nil?
-		#end
 	end
 
 	return results
