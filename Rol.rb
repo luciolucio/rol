14267ff8b4b6d7982af7506e038b8ca7aeccb36c
Lucio Assis <luciolucio@gmail.com>
2013-01-26 19:21:06 -0200

    Implementei gerenciamento de processed/unprocessed

--- Rol.rb
+++ Rol.rb
@@ -2,70 +2,83 @@
 
 require 'yaml'
 require 'bigdecimal'
-require 'mail'
-require_relative 'lib/mailer' 
-require_relative 'lib/processor' 
+require 'gmail'
 
-def read_text_from_file
-	file = File.new( "out3.txt" )
-	full_text = file.read
-
-	return [ full_text ]
-end
-
-def get_text_for_messages
-	return read_text_from_file
-
-	config = YAML.load( File.new( 'config.y' ) )
-
-	Mail.defaults do
-		retriever_method :imap, {
-			:address    => 'imap.gmail.com',
-			:port       => 993,
-			:enable_ssl => true,
-			:user_name  => config[ :user ],
-			:password   => config[ :pass ],
-		}
+class Message
+	def initialize( message )
+		@message = message
 	end
 
-	emails = Rol::Mailer.get_all
-	emails.map do | email |
-		email[ :body ]
+	def archive!
+		@message.move_to( "processed" )
+	end
+
+	# Delegate all other methods to the Mail message
+	def method_missing(*args, &block)
+		if block_given?
+			@message.send(*args, &block)
+		else
+			@message.send(*args)
+		end
 	end
 end
 
-def parse( messages )
-	results = []
+class Mailbox
+	@@session = nil
 
-	messages.each do | full_text |
-		bandeira = full_text.scan( /CARTAO PERSONNALITE (.*) PLATINUM/ )[ 0 ]
-		lines = full_text.split( "\n" )
+	def self.initialize
+		if @@session.nil?
+			config = YAML.load( File.new( 'config.y' ) )
+			session = Gmail.new( config[ :user ], config[ :pass ] )
+			@@session = session
+		end
+	end
 
-		cartao_final = 0
+	def self.get_all_unprocessed
+		self.initialize
 
-		lines.each do | line |
-			numero = line.scan( /final ...(\d*)/ )[ 0 ]
-			if numero.nil? && cartao_final == 0
+		return @@session.mailbox( "unprocessed" ).emails(
+			:from => "itau-unibanco.com.br",
+		).collect { | m | Message.new( m ) }
+	end
+end
+
+def filter( messages )
+	messages.keep_if do | m |
+		m.subject.include? "ltimas trans" and m.subject.include? "realizadas com o cart"
+	end
+end
+
+def parse( message )
+	parsed = [] 
+
+	full_text = message.text_part.body.decoded
+	bandeira = full_text.scan( /CARTAO PERSONNALITE (.*) PLATINUM/ )[ 0 ]
+	lines = full_text.split( "\n" )
+
+	cartao_final = 0
+
+	lines.each do | line |
+		numero = line.scan( /final ...(\d*)/ )[ 0 ]
+		if numero.nil? && cartao_final == 0
+			next
+		elsif !numero.nil?
+			cartao_final = numero
+		else
+			entrada = line.scan( /^(\d\d-\d\d-\d\d\d\d)\s+(.*?)\s+R\$\s([0-9,.]+).*$/ )[ 0 ]
+			if entrada.nil?
 				next
-			elsif !numero.nil?
-				cartao_final = numero
 			else
-				entrada = line.scan( /^(\d\d-\d\d-\d\d\d\d)\s+(.*?)\s+R\$\s([0-9,.]+).*$/ )[ 0 ]
-				if entrada.nil?
-					next
-				else
-					data, estabelecimento, valor = entrada
-					data = Date.parse( data )
-					valor = BigDecimal.new( valor.gsub( ",", "virgula" ).gsub( ".", "," ).gsub( "virgula", "." ) )
-					result = { "bandeira" => bandeira, "cartao" => cartao_final, "data" => data, "estab" => estabelecimento, "valor" => valor }
-
-					results.push result
-				end
+				data, estabelecimento, valor = entrada
+				data = Date.parse( data )
+				valor = BigDecimal.new( valor.gsub( ",", "virgula" ).gsub( ".", "," ).gsub( "virgula", "." ) )
+				result = { "bandeira" => bandeira, "cartao" => cartao_final, "data" => data, "estab" => estabelecimento, "valor" => valor }
+				parsed.push result
 			end
 		end
 	end
 
-	return results
+	parsed
 end
 
 def main
@@ -76,13 +89,16 @@ def main
 	#}
 	#tags = tag_map[ estabelecimento ]
 
-	messages = get_text_for_messages
-	parsed = parse( messages )
+	messages = Mailbox.get_all_unprocessed
+	messages = filter( messages )
 
-	parsed.each do | item |
-		puts item
+	messages.each do | message |
+		parsed = parse( message )
+		message.archive!
+		parsed.each do | p |
+			puts p
+		end
 	end
-	
 end
 
 main
