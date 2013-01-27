e070eaf3ee4e0712bc48b38223088827866f2ecb
Lucio Assis <luciolucio@gmail.com>
2013-01-27 16:52:24 -0200

    Movi coisas para a classe Expense

--- bin/Rol.rb
+++ bin/Rol.rb
@@ -12,57 +12,22 @@ def filter( messages )
 	end
 end
 
-def parse( message )
-	parsed = [] 
-
-	full_text = message.text_part.body.decoded
-	card_type = full_text.scan( /CARTAO PERSONNALITE (.*) PLATINUM/ ).first.first
-	lines = full_text.split( "\n" )
-
-	card_no = 0
-
-	lines.each do | line |
-		number = line.scan( /final ...(\d*)/ ).first
-		if number.nil? && card_no == 0
-			next
-		elsif !number.nil?
-			card_no = number.first
-		else
-			entry = line.scan( /^(\d\d-\d\d-\d\d\d\d)\s+(.*?)\s+R\$\s([0-9,.]+).*$/ ).first
-			if entry.nil?
-				next
-			else
-				date, seller, value = entry
-				date = Date.parse( date )
-				value = BigDecimal.new( value.gsub( ",", "virgula" ).gsub( ".", "," ).gsub( "virgula", "." ) )
-				result = Expense.new( card_type, card_no, date, seller, value )
-				parsed.push result
-			end
-		end
-	end
-
-	parsed
-end
 
 def main
-	#tag_map = {
-		#"EXTRA 1341 SA" => [ "#mercado", "#extraitaim" ],
-		#"R M R COM DE GAS SA" => [ "#gas" ],
-		#"RESTAURANTE CHOUPANA OL" => [ "#alimentacao", "#viagem" ],
-	#}
-	#tags = tag_map[ seller ]
-
 	config = YAML.load( File.new( '../config/config.y' ) )
 	user = config[ :user ]
 	password = config[ :pass ]
+
 	mbox = Mailbox.new( user, password )
 	messages = filter( mbox.get_all_unprocessed )
 
 	messages.each do | message |
-		parsed = parse( message )
-		message.archive!
-		parsed.each do | p |
-			puts p.inspect
+		full_text = message.text_part.body.decoded
+		expenses = Expense.parse( full_text )
+		#message.archive!
+
+		expenses.each do | e |
+			puts e.inspect
 		end
 	end
 end
--- lib/expense.rb
+++ lib/expense.rb
@@ -1,4 +1,18 @@
 class Expense
+	@@tag_map = {
+		"EXTRA 1341 SA"           => [ "#mercado", "#extraitaim" ],
+		"R M R COM DE GAS SA"     => [ "#gas"                    ],
+		"RESTAURANTE CHOUPANA OL" => [ "#alimentacao", "#viagem" ],
+	}
+
+	@@description_map = {
+		"EXTRA 1341 SA"           => "Extra Itaim",
+		"R M R COM DE GAS SA"     => "Gas",
+		"RESTAURANTE CHOUPANA OL" => "Restaurante em Recife",
+	}
+
+	attr_reader :card_type, :card_no, :date, :seller, :value
+
 	def initialize( card_type, card_no, date, seller, value )
 		@card_type = card_type
 		@card_no = card_no
@@ -8,6 +22,47 @@ class Expense
 	end
 
 	def inspect
-		"#<#{self.class}:#{self.object_id}, #{@card_type} #{@card_no} #{@date} #{@seller} %.2f>" % @value
+		"#<#{self.class}:#{self.object_id}, #{@card_type} #{@card_no} #{@date} #{self.description} %.2f #{self.tags}>" % @value
+	end
+
+	def description
+		@@description_map[ @seller ] || @seller
+	end
+
+	def tags
+		@@tag_map[ @seller ] || []
+	end
+
+	class << self
+		def parse( full_text )
+			parsed = [] 
+
+			card_type = full_text.scan( /CARTAO PERSONNALITE (.*) PLATINUM/ ).first.first
+			lines = full_text.split( "\n" )
+
+			card_no = 0
+
+			lines.each do | line |
+				number = line.scan( /final ...(\d*)/ ).first
+				if number.nil? && card_no == 0
+					next
+				elsif !number.nil?
+					card_no = number.first
+				else
+					entry = line.scan( /^(\d\d-\d\d-\d\d\d\d)\s+(.*?)\s+R\$\s([0-9,.]+).*$/ ).first
+					if entry.nil?
+						next
+					else
+						date, seller, value = entry
+						date = Date.parse( date )
+						value = BigDecimal.new( value.gsub( ",", "virgula" ).gsub( ".", "," ).gsub( "virgula", "." ) )
+						result = Expense.new( card_type, card_no, date, seller, value )
+						parsed.push result
+					end
+				end
+			end
+
+			parsed
+		end
 	end
 end
