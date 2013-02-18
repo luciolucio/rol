7f2022fd98b7ec47ac47dee6ff060c9a25a340a0
Lucio Assis <luciolucio@gmail.com>
2013-02-17 23:52:16 -0300

    Adding new config file

---
+++ config/config_template.rb
@@ -0,0 +1,11 @@
+Configuration.for( "config" ) do
+	db {
+		url  "<db_url>"
+		name "<db_name>"
+	}
+
+	gmail {
+		login    "<gmail_user>"
+		password "<gmail_password>"
+	}
+end
--- config/config_template.y
+++
@@ -1,4 +0,0 @@
-:email_user: <email_user>
-:email_passwd: <email_passwd>
-:db_url: <db_url>
-:db_name: <db_name>
