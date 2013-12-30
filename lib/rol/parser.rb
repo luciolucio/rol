019e8d67290e10cb30b428d9ec8d5f3290159fed
Lucio Assis <luciolucio@gmail.com>
2013-12-29 23:55:45 -0700

    Fix the parser again

--- lib/rol/parser.rb
+++ lib/rol/parser.rb
@@ -5,7 +5,7 @@ module Rol
   # parsed values such as quantity and description
 
   class Parser
-    @nil_result = { amount: 0, description: '' }
+    @@nil_result = { amount: 0, description: '' }
 
     def parse(message)
       results = /A \$(.*) (debit card transaction to (.*)|ATM withdrawal) on/.match(message)
@@ -14,7 +14,7 @@ module Rol
         results = /A \$(.*) (external transfer to (.*)|ATM withdrawal) on/.match(message)
       end
 
-      return @nil_result if results.nil?
+      return @@nil_result if results.nil?
 
       amount = results[1].to_f
       description = results[2] == 'ATM withdrawal' ? 'ATM Withdrawal' : results[3].strip
