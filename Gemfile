a386bad5122d6e2fb869efc3a9b2fe76dd7166e0
Lucio Assis <luciolucio@gmail.com>
2014-06-03 20:40:23 -0600

    Goodbye ruby-gmail. And other dependencies I wasn't using anymore

--- Gemfile
+++ Gemfile
@@ -1,5 +1,4 @@
 source 'https://rubygems.org'
 
-gem 'ruby-gmail', require: 'gmail'
-gem 'rr', require: false
-gem 'american_date', require: false
+gem 'american_date'
+gem 'mail'
--- lib/rol.rb
+++ lib/rol.rb
@@ -5,7 +5,6 @@ require 'bundler/setup'
 require_relative 'rol/config'
 require_relative 'rol/expense'
 require_relative 'rol/extensions/message'
-require_relative 'rol/gmailconnection'
 require_relative 'rol/messages'
 
 require_relative 'rol/rol'
